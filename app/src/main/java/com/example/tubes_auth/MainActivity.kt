fd9e8ef909b88a93dfe23d2f3a073a5263341393
Adyuta Indra Adyatma <adyuta24.hr@gmail.com>
2023-11-23 22:48:10 +0700

    added splash screen

--- app/src/main/java/com/example/tubes_auth/MainActivity.kt
+++ app/src/main/java/com/example/tubes_auth/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.tubes_auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.example.tubes_auth.R
+import kotlinx.coroutines.delay
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        Handler().postDelayed({
+            val intent = Intent(this, WelcomeLogin::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
+
     }
 }
