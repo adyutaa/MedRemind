43e4e6ce2b51870b163c5f4b1b4004d40dccb932
tanziilaag <119391821+tanziilaag@users.noreply.github.com>
2023-11-24 15:39:25 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/tubes_auth/EditMedMenu.kt
@@ -0,0 +1,11 @@
+package com.example.tubesmedremind
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditMedMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_med_menu)
+    }
+}
---
+++ app/src/main/java/com/example/tubes_auth/MedMenu.kt
@@ -0,0 +1,11 @@
+package com.example.tubesmedremind
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MedMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_med_menu)
+    }
+}
